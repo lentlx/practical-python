77b2d4086db26f4e3211e1f3564a06a6546439ae
lentlx <lennylen4@gmail.com>
2021-09-15 12:37:51 +0200

    add exo1 + 2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,11 +1,21 @@
 # bounce.py
 #
 # Exercise 1.5
-heightDrop = 100
+height_drop = 100
 bounce = 1
 
 while bounce < 10:
-    heightDrop = heightDrop * 3 / 5
-    print(bounce, round(heightDrop))
+    height_drop = height_drop * 3 / 5
+    print(bounce, round(height_drop))
     bounce = bounce + 1
 
+# num_bills = 1
+# sears_height = 443
+# bill_thickness = 0.11 * 0.001
+# day = 1
+
+# while num_bills * bill_thickness < sears_height:
+#     print(day, num_bills * bill_thickness)
+
+#     day = day + 1
+#     num_bills = num_bills * 2
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 1
+extra_payment_end_month = 13
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+
+    if extra_payment_start_month <= month < extra_payment_end_month:
+        current_payment = payment + extra_payment
+    else:
+        current_payment = payment
+
+    if principal < current_payment:
+        current_payment = principal * (1 + rate / 12)
+
+    principal = principal * (1 + rate / 12) - current_payment
+    total_paid = total_paid + current_payment
+
+    print(month, total_paid, principal)
