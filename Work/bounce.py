b21e0b743af945f8e400ccce8b463fc060a82c87
lentlx <lennylen4@gmail.com>
2021-09-15 10:28:03 +0200

    Add exo1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+heightDrop = 100
+bounce = 1
+
+while bounce < 10:
+    heightDrop = heightDrop * 3 / 5
+    print(bounce, round(heightDrop))
+    bounce = bounce + 1
+
